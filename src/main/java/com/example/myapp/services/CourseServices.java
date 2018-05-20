be773c9a77fc092b60ea54b5cc9992265c8be516
NikithaKoppu <35382140+NikithaKoppu@users.noreply.github.com>
2018-05-20 14:42:34 -0400

    imported Restcontroller in course svc

--- src/main/java/com/example/myapp/services/CourseServices.java
+++ src/main/java/com/example/myapp/services/CourseServices.java
@@ -2,10 +2,12 @@ package com.example.myapp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.myapp.model.Course;
 import com.example.myapp.repositories.CourseRepository;
 
+@RestController
 public class CourseServices {
 	@Autowired
 	CourseRepository courseRepository;	
