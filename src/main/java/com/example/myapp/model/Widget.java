3f8ea2af33c12b471f1409cacf6a253ce7bb6660
NikithaKoppu <35382140+NikithaKoppu@users.noreply.github.com>
2018-05-26 22:20:50 -0400

    added Widget data model

---
+++ src/main/java/com/example/myapp/model/Widget.java
@@ -0,0 +1,27 @@
+package com.example.myapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Widget {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String text;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+}
